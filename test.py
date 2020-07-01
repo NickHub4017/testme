be65812ae2ce708d7c1fc16408f3c00caf06c15e
NickHub4017 <nrv.rox.u@gmail.com>
2020-07-01 07:02:27 +0530

    A

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import pymysql
 import time
-
+print("aaaaaaa")
 while 1:
     # Open database connection
+    print("bbbbb")
     db = pymysql.connect("database-3.ctvdurq9dsmz.us-east-2.rds.amazonaws.com","root","eranga19covid","covid_19" )
-
+    print("ccccc")
     # prepare a cursor object using cursor() method
     cursor = db.cursor()
-
+    print("dddddd")
     # execute SQL query using execute() method.
     cursor.execute("show tables")
-
+    print("eeeeee")
     # Fetch a single row using fetchone() method.
     data = cursor.fetchall()
     print (data)
