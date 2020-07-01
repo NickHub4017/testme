5a3b127b26cd62878faac97651e197df395ef98e
NickHub4017 <nrv.rox.u@gmail.com>
2020-07-01 07:29:41 +0530

    A

--- test.py
+++ test.py
@@ -1,21 +1,33 @@
 import pymysql
 import time
 print("aaaaaaa")
-while 1:
-    # Open database connection
-    print("bbbbb")
-    db = pymysql.connect("database-3.ctvdurq9dsmz.us-east-2.rds.amazonaws.com","root","eranga19covid","covid_19" )
-    print("ccccc")
-    # prepare a cursor object using cursor() method
-    cursor = db.cursor()
-    print("dddddd")
-    # execute SQL query using execute() method.
-    cursor.execute("show tables")
-    print("eeeeee")
-    # Fetch a single row using fetchone() method.
-    data = cursor.fetchall()
-    print (data)
 
-    # disconnect from server
-    db.close()
-    time.sleep(55)
+# Open database connection
+print("bbbbb")
+db = pymysql.connect("database-3.ctvdurq9dsmz.us-east-2.rds.amazonaws.com","root","eranga19covid","covid_19" )
+print("ccccc")
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+print("dddddd")
+# execute SQL query using execute() method.
+cursor.execute("show tables")
+print("eeeeee")
+# Fetch a single row using fetchone() method.
+data = cursor.fetchall()
+print (data)
+
+# disconnect from server
+db.close()
+
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
